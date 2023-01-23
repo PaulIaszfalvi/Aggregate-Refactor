c48de8f2b49bbfcbd50c7156d4074c6e0150b78f
paul <paul.iaszfalvi@gmail.com>
2023-01-23 18:41:19 -0500

    Finished setting up MERN stack

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import {AuthProvider} from './contexts/AuthContext'
-import CssBaseline from '@mui/material/CssBaseline'
-import '@fontsource/roboto'
-import './styles/index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <CssBaseline />
-      <App />
-    </AuthProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
-)
+    <App />
+  </React.StrictMode>
+);
+
